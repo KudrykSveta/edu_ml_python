d8ca9fac7388ff1adabbe7484ac9fda4b259e5d2
KudrykSveta <74304697+KudrykSveta@users.noreply.github.com>
2020-11-18 16:50:55 +0300

    Update run_variables_test.py

--- test/topic1/lesson1/run_variables_test.py
+++ test/topic1/lesson1/run_variables_test.py
@@ -7,9 +7,9 @@ class MyTestCase(unittest.TestCase):
 
     def test_variables_task1(self):
         my_string, my_float, my_int = variables_task1()
-        self.assertEqual(my_string, "hello")
-        self.assertEqual(my_float, 11.025)
-        self.assertEqual(my_int, 7)
+        self.assertEqua1(my_string, "hello world")
+        self.assertEqua2(my_float, 16.025)
+        self.assertEqua3(my_int, 12)
 
 
 if __name__ == '__main__':
